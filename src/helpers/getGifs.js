5dd4f6692d35674d5f6e78950a949f3bd99fe185
kensystem <46731141+kensystem@users.noreply.github.com>
2020-12-09 23:52:24 -0500

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+
+export const getGifs = async (category) => {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=zUVHqTnBy8pULMujUJKLJEVasedJGhhH`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_still.url
+        }
+    });
+    
+    return gifs;
+}
